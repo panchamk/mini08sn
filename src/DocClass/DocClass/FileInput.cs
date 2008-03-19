15f63bf3dbc6574e4f734381f44bd0aea7c90a33
bilski.tomek <bilski.tomek@9203d4f6-6547-0410-ac26-ef3882dbe8ae>
2008-03-19 05:22:49 +0000

    kontrolka do pobierania pliku

---
+++ src/DocClass/DocClass/FileInput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DocClass
+{
+    public partial class FileInput : UserControl
+    {
+        public FileInput()
+        {
+            InitializeComponent();
+        }
+
+        private void FileInput_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

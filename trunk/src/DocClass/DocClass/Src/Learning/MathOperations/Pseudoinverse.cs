52f377ae49368f5001f2c2ac40c854f2d789cd4c
ziober_m@interia.pl <ziober_m@interia.pl@9203d4f6-6547-0410-ac26-ef3882dbe8ae>
2008-04-02 17:23:44 +0000

    Zmiana implementacji lagorytmu SVD

--- trunk/src/DocClass/DocClass/Src/Learning/MathOperations/Pseudoinverse.cs
+++ trunk/src/DocClass/DocClass/Src/Learning/MathOperations/Pseudoinverse.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MatrixBuild = dnAnalytics.LinearAlgebra.MatrixBuilder;
+using MatrixDN = dnAnalytics.LinearAlgebra.Matrix;
 
 namespace DocClass.Src.Learning.MathOperations
 {
@@ -18,7 +20,7 @@ namespace DocClass.Src.Learning.MathOperations
         /// <param name="matrix">Macierz M.</param>
         /// <returns>Macierz M+</returns>
         /// <exception cref="NullReferenceException">Jesli argument bedzie null.</exception>
-        /// <exception cref="InvalidOperationException">Jesli macierz nie uda sie rozlozyc.</exception>
+        /// <exception cref="InvalidOperationException">Jesli macierz nie uda sie rozlozyc.</exception>        
         public static double[,] Solve(double[,] matrix)
         {
             if (matrix == null)
@@ -27,7 +29,7 @@ namespace DocClass.Src.Learning.MathOperations
             double[,] u = new double[0, 0];
             double[,] vt = new double[0, 0];
             double[] w = new double[0];
-
+            matrix = Matrix.ZeroComplete(matrix);
             if (!SVD.Decomposition(matrix, ref w, ref u, ref vt))
                 throw new InvalidOperationException("Macierz nie udalo sie rozlozyc !");
 
@@ -37,11 +39,76 @@ namespace DocClass.Src.Learning.MathOperations
             sigma = Matrix.Reciprocal(sigma);
             double[,] matrixPlus = Matrix.Multiply(vMatrix, sigma);
             matrixPlus = Matrix.Multiply(matrixPlus, uTrans);
-
             u = vt = sigma = uTrans = vMatrix = null;
-            return matrixPlus;
+            
+            return matrixPlus = Matrix.RemoveFirstColumnAndFirstRow(matrixPlus);
         }
 
+        #endregion
+
+        #region Private Methods
+        /*
+        private static double[,] Solve(double[,] matrix)
+        {
+            double[,] u = null, vt = null;
+            double[] w = null;
+            if (!SVD.Decomposition(matrix, ref w, ref u, ref vt))
+                throw new InvalidOperationException("Macierz nie udalo sie rozlozyc !");
+            //tworze macierz w
+            MatrixDN matrixW = CreateMatrix(w, matrix.GetLength(0), matrix.GetLength(1));
+            //tworze macierz u
+            MatrixDN matrixU = CreateMatrix(u);
+            //tworze macie vt
+            MatrixDN matrixVT = CreateMatrix(vt);
+            //transpozycja u
+            matrixU = matrixU.Transpose();
+            //transozycja Vt -> V
+            matrixVT = matrixVT.Transpose();
+            //transpozycja polaczona z odwronoscia kazdego niezerowego elementu
+            matrixW = Reciprocal(matrixW);
+            //mplus
+            MatrixDN mPlus = (matrixVT * matrixW) * matrixU;
+            //tablica
+            return mPlus.ToArray();
+        }
+
+        private static MatrixDN CreateMatrix(double[] diagonal, int lenght0, int lenhgt1)
+        {
+            MatrixDN matrixW = MatrixBuild.CreateMatrix(lenght0 - 1, lenhgt1 - 1);
+            for (int i = 1; i < lenght0; i++)
+                for (int j = 1; j < lenhgt1; j++)
+                {
+                    if (i == j && i < diagonal.Length)
+                        matrixW[i - 1, j - 1] = diagonal[i];
+                    else
+                        matrixW[i - 1, j - 1] = 0;
+                }
+            return matrixW;
+        }
+
+        private static MatrixDN CreateMatrix(double[,] m)
+        {
+            MatrixDN matrixW = MatrixBuild.CreateMatrix(m.GetLength(0) - 1, m.GetLength(1) - 1);
+            for (int i = 1; i < m.GetLength(0); i++)
+                for (int j = 1; j < m.GetLength(1); j++)
+                {
+                    matrixW[i - 1, j - 1] = m[i, j];
+                }
+            return matrixW;
+        }
+
+        private static MatrixDN Reciprocal(MatrixDN matrix)
+        {
+            matrix = matrix.Transpose();
+            for (int j = 0; j < matrix.Columns; j++)
+                for (int i = 0; i < matrix.Rows; i++)
+                {
+                    if (matrix[i, j] != 0)
+                        matrix[i, j] = 1 / matrix[i, j];
+                }
+            return matrix;
+        }
+        */
         #endregion
     }
 
